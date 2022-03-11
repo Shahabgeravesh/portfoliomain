d1dda4614bea3a403166da9378b7e8f5adb03776
Shahabgeravesh <Shahabgeravesh@gmail.com>
2022-03-10 23:32:15 -0800

    fixed nav buf

--- src/screens/navigationBar/NavigationBar.js
+++ src/screens/navigationBar/NavigationBar.js
@@ -17,7 +17,9 @@ function NavigationBar() {
     return (
         <div id="home">
 
-            <Navbar className="navigation-container" expand="md">
+            <Navbar className="navigation-container"
+                style={{ zIndex: '2', position: 'fixed', top: '0', width: '100%' }} collapseOnSelect
+                expand="md">
                 <Navbar.Brand style={{ marginLeft: '1rem' }} href="#home">Shahab Geravesh</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse style={{ justifycontent: 'flex-end', marginRight: '1rem', bordercolor: 'none' }}>
