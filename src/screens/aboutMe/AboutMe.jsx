f43057733672f25749a0daf63b733f4f3510c1a4
Shahabgeravesh <Shahabgeravesh@gmail.com>
2022-03-13 21:53:07 -0700

    about me restructure

--- src/screens/aboutMe/AboutMe.jsx
+++ src/screens/aboutMe/AboutMe.jsx
@@ -4,15 +4,17 @@ import Avatar from '../../images/Avatar.jpg'
 
 function AboutMe() {
     return (
-        <div className="about__container" id="about-me">
+        <div id="about-me">
             <div>
-                <img src={Avatar} alt="Shahab Avatar" />
+                <h1 className="about-me">ABOUT ME</h1>
 
             </div>
-            <div>
-                <h1 className="about__text">ABOUT ME</h1>
-                <h5>I'm a Data Scientist and a Fullstack Web Developer. My Interests are building new Web Technologies and Products, Data Analytics, Machine Learning, and Deep Learning. I apply my passion for developing products with MERN Stack.</h5>
+            <div className="about__container">
+                <img src={Avatar} alt="Shahab Avatar" />
+                <h5 ClassName="about-text">I'm a Data Scientist and a Fullstack Web Developer. My Interests are building new Web Technologies and Products, Data Analytics, Machine Learning, and Deep Learning. I apply my passion for developing products with MERN Stack.</h5>
+
             </div>
+
 
 
         </div>
