7a1d59b8602a77d3adf624207a25e4c10750f456
Shahabgeravesh <Shahabgeravesh@gmail.com>
2022-03-13 21:19:32 -0700

    deleted some extra files

--- src/helpers/portfolioData.js
+++ src/helpers/portfolioData.js
@@ -3,23 +3,19 @@ import Goog from '../images/projectsicon/Goog.jpg';
 import SuperStore from '../images/projectsicon/SuperStore .jpg';
 import Product from "../images/projectsicon/Product.jpg";
 import Countrywise_sales from "../images/projectsicon/Countrywise_Sales.png";
-import tindog from "../images/projectsicon/tindog.png";
-import rememberSeq from "../images/projectsicon/remember-seq.png";
-import drumkit from "../images/projectsicon/drumkit.png";
-import interactiveparticles from "../images/projectsicon/interactiveparticles.gif";
-import generativeArt from "../images/projectsicon/generative_art.png";
-import gorjeo from "../images/projectsicon/gorjeo3.jpg";
 
 
 //add project on top for latest
 
 const portfolioData = [
+
+
   {
-    image: Goog,
-    link: 'https://bookserachenginemenstack.herokuapp.com/',
-    desc: 'Search for Books Mern Stack App',
-    summary: ' Used Google Books API to create the Search For Books application. Users can sign up and log into the account, search for a book of their liking, and add it to a list of saved books, users can also remove the saved books. ',
-    tech: 'JavaScript, Node.js, React, Express, MongoDB, Apollo Server, GraphQL, Dotenv, Mongoose '
+    image: Product,
+    link: 'https://public.tableau.com/app/profile/shahabgeravesh#!/?newProfile=&activeTab=0',
+    desc: 'Tableau Data Visualization',
+    summary: 'Tableau Data Visualization',
+    tech: 'Tableau'
   },
   {
     image: SuperStore,
@@ -29,11 +25,11 @@ const portfolioData = [
     tech: 'Tableau'
   },
   {
-    image: Product,
-    link: 'https://public.tableau.com/app/profile/shahabgeravesh#!/?newProfile=&activeTab=0',
-    desc: 'Tableau Data Visualization',
-    summary: 'Tableau Data Visualization',
-    tech: 'Tableau'
+    image: Goog,
+    link: 'https://bookserachenginemenstack.herokuapp.com/',
+    desc: 'Search for Books Mern Stack App',
+    summary: ' Used Google Books API to create the Search For Books application. Users can sign up and log into the account, search for a book of their liking, and add it to a list of saved books, users can also remove the saved books. ',
+    tech: 'JavaScript, Node.js, React, Express, MongoDB, Apollo Server, GraphQL, Dotenv, Mongoose '
   },
   {
     image: Countrywise_sales,
@@ -42,6 +38,8 @@ const portfolioData = [
     summary: 'Tableau Data Visualization',
     tech: 'Tableau'
   },
+
+
 
 
 
